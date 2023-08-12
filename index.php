1ca0e49fa6553b20c7ff2dab6e93ab75713e0913
zahernaher <74210661+zahernaher@users.noreply.github.com>
2023-08-12 18:34:13 +0300

    fullstack_complete

--- index.php
+++ index.php
@@ -13,22 +13,14 @@ require_once 'WeatherAPI.php';
  <body onload="load()"> 
  
 	<div class="wrap">
-		<form id="myForm" action="<?php $_SERVER['REQUEST_URI'] ?>" method="get" name="frm1">
-			
+		<form id="myForm" action="<?php $_SERVER['REQUEST_URI'] ?>" method="get" name="frm1">			
 			<div class="search-form" id="search-form">
 				<input class="search_form_input" type="text" name="city" id="" value=<?php echo $city ?>>
 				<input class="search_form_send" id="doGo" type="submit" name="doGo" value="OK">
 			</div> 
 
-			<div class="options_wrap">
-				
-				<div class="selected_sity"><?php echo $city ?></div>
-				
-
-			
-
-
-				
+			<div class="options_wrap">				
+				<div class="selected_sity"><?php echo $city ?></div>				
 				<div class="switch_wrap">
 					<sub>O  &nbsp</sub>
 					<label class="switch" for="check" >
@@ -37,22 +29,28 @@ require_once 'WeatherAPI.php';
 						<div class="switch_slider_F">F</div>					
 					</label>
 				</div>
-				
-				
-				
-
 			</div>
-			<div class="change_sity" >Сменить город</div>
-			<input class="search_form_send" id="doGoGeo" type="submit" name="doGoGeo" value="Моё местоположение">
-			     
+
+			<div class="change_sity_wrap">
+				<div class="change_sity" >
+					<span>Сменить город</span>					
+				</div>
+				<label class="doGoGeo" for="doGoGeo">
+					<svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path fill-rule="evenodd" clip-rule="evenodd" d="M22.489 5.83819L5.23895 16.6268L13.728 18.2769L18.2146 25.7637L22.489 5.83819Z" fill="white" fill-opacity="0.4"/>
+					</svg>
+					<input class="change_sity_by_ip" id="doGoGeo" type="submit" name="doGoGeo" value="Моё местоположение">
+				<label for=""></label>
+			</div>			     
     </form>
+		
 		<div class="main">
 			<div class="temp_wrap">
 				<div class="temp_info">
 					<div id="icon"><img id="wicon" src=<?php echo $weatherIconImg ?>></div>		
 					<div class="temp">
 						<span class="temp_value"><?php echo $temperatur ?></span>
-						<!-- <span class="temp_units"></span> -->
+						
 					</div>
 				</div>
 			</div>
--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", (e) => {
 	document.querySelector('.change_sity').addEventListener('click', (e) =>{
 		document.getElementById('search-form').classList.add('search-form_show');
+		document.querySelector('.change_sity_wrap').classList.add('change_sity_wrap_opsity');
 	})
 
 	document.querySelector('.switch_input').addEventListener('click', (e) =>{
 		document.getElementById('search-form').classList.remove('search-form_show');
-	})
-	
-	// Checker color
-	
-
+		document.querySelector('.change_sity_wrap').classList.remove('change_sity_wrap_opsity');
+	})	
 	
 });
-
 
 function switch_units(){	
 
